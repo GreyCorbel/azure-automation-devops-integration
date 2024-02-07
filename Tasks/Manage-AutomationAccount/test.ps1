1eae7d6cf946fd039cae96e9b8f68c58dad7a86e
RomanSebranek <roman.sebranek@dabis.cz>
2024-02-07 14:39:42 +0100

    efg

--- Tasks/Manage-AutomationAccount/test.ps1
+++ Tasks/Manage-AutomationAccount/test.ps1
@@ -3,5 +3,7 @@ $scriptDirectory = Split-Path $script:MyInvocation.MyCommand.Path
 $scriptDirectory
 
 # Create absolute path to the task directory
-$taskDirectory = Join-Path $scriptDirectory "Tasks\$taskDirectoryName"
+$taskDirectory = Join-Path $scriptDirectory "Tasks\Manage-AutomationAccount"
 $taskDirectory
+
+Set-Location $taskDirectory
